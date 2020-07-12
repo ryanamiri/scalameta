dadc0912be8e86d031304b466be80a70b9c5ad84
Ryan Amiri <ryanamiri@quantexa.com>
2020-07-12 18:59:23 +0100

    Almost there

--- src/main/scala/practice/mappable.scala
+++ src/main/scala/practice/mappable.scala
@@ -2,22 +2,27 @@ package practice
 
 import scala.annotation.StaticAnnotation
 import scala.meta._
+import scala.meta.inputs.Input.String
 
 class mappable extends StaticAnnotation {
   inline def apply(defn: Any): Any = meta {
-    val q"..$mods class $tName (..$params) extends $template" = defn
-
-    val res =
-      q"""
-      ..$mods class $tName(..$params) {
-        def toMap(): Map[String, Any] = Map[String, Any]()
-      }
-    """
-
-    println("============== result ==============")
-    println(res)
-    println("====================================")
-    res
+    val taggedAs = Term.Name("taggedAs")
+    val tagSlowTest = Term.Apply(Term.Name("Tag"), List(Lit("SlowTest")))
+    defn match {
+      case c @ Defn.Class(_, _, _, _, Template(_, _, _, Some(t))) =>
+        val s = t.toList.map(x => {
+          x match {
+            case w @ Term.Apply(Term.Select(Term.Apply(Term.Select(u1, u2), Seq(v)), a), b) if
+            u2.value != "taggedAs" =>
+              println(w.structure)
+              val aa = Term.Apply(Term.Select(u1, u2), List(v))
+              Term.Apply(Term.Select(Term.Apply(Term.Select(aa, taggedAs), List(tagSlowTest), a), b)
+            case _ => x
+          }
+        })
+        c.copy(templ = c.templ.copy(stats = Some(s)))
+      case _ => defn
+    }
   }
 }
 
--- src/test/scala/practice/MacrosTest.scala
+++ src/test/scala/practice/MacrosTest.scala
@@ -1,14 +1,21 @@
 package practice
 
+import org.scalatest.Tag
 import org.scalatest.flatspec.AnyFlatSpecLike
 import org.scalatest.matchers.should.Matchers
 
+@mappable
 class MacrosTest extends AnyFlatSpecLike with Matchers {
 
-  "testFunction" should "return it's input" in {
-    TestClass(1).toMap() shouldEqual Map.empty[String, Any]
+  "testFunction" should "return it's input" taggedAs Tag("SlowTest") in {
+  }
+
+  it should "something" in {
+  }
+
+  "something" should "something else" taggedAs Tag("SlowTest") in {
   }
 }
 
-@mappable
-case class TestClass(a: Int)
+//@mappable
+//case class TestClass(a: Int)
